5c39c4aadbf92ad41b377b1333ff071a4971a948
Amir Sakib Saad <amirsakib16@gmail.com>
2026-01-01 21:03:16 +0600

    link

--- frontend/qna/src/App.jsx
+++ frontend/qna/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     setResult(null);
     
     try {
-      const response = await fetch('http://127.0.0.1:5000/predict', {
+      const response = await fetch('https://nlpqnaclassifier440.onrender.com/predict', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ text: inputText }),
