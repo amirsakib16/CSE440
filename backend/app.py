392376db5c4923f77ca27bea4d0bb50bc73ff4d0
Amir Sakib Saad <amirsakib16@gmail.com>
2026-01-02 14:50:10 +0600

    djgd55555

--- backend/app.py
+++ backend/app.py
@@ -32,7 +32,7 @@ app = Flask(
 CORS(app, resources={
     r"/*": {
         "origins": [
-            "https://cse440amirsakib.onrender.com",
+            " https://cse440-5.onrender.com",
             "http://localhost:5173",  # for local dev
             "http://localhost:4173"
         ],
