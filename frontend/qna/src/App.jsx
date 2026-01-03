01716a1433fdd9fe0fb6ecb818cc8f46dd184ab6
Amir Sakib Saad <amirsakib16@gmail.com>
2026-01-03 17:14:45 +0600

    v

--- frontend/qna/src/App.jsx
+++ frontend/qna/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [error, setError] = useState('');
 
   // Use environment variable or relative URL for production
-const API_URL = "http://127.0.0.1:5000";
+const API_URL = "";
 
 const handlePredict = async () => {
   setLoading(true);
